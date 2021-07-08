865128b5ab36026f240e90fb18141dea085d9ce0
Warda Nur <wardanur04@gmail.com>
2021-07-08 19:27:00 +0800

    slack-clone: test carl

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -11,18 +11,18 @@ const initialState = {
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
-export const incrementAsync =
-  createAsyncThunk();
-  //   // "counter/fetchCount",
-  //   async (amount) => {
-  //     const response = await fetchCount(amount);
-  //     // The value we return becomes the `fulfilled` action payload
-  //     return response.data;
-  //   }
+export const incrementAsync = createAsyncThunk();
+//   // "counter/fetchCount",
+//   async (amount) => {
+//     const response = await fetchCount(amount);
+//     // The value we return becomes the `fulfilled` action payload
+//     return response.data;
+//   }
 
-export const counterSlice = createSlice({
-  name: "counter",
+export const appSlice = createSlice({
+  name: "app",
   initialState,
+  roomId: null,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     increment: (state) => {
@@ -54,12 +54,12 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = appSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => state.app.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
@@ -70,4 +70,4 @@ export const incrementIfOdd = (amount) => (dispatch, getState) => {
   }
 };
 
-export default counterSlice.reducer;
+export default appSlice.reducer;
